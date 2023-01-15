d5a2573d2dfa19cf6c493a8cbcce3a889cd4cd17
muhamed-hassan <mohamed.hassan.work@gmail.com>
2023-01-15 08:50:07 +0200

    add lesson 12

---
+++ src/com/lesson12/Launcher.java
@@ -0,0 +1,15 @@
+package com.lesson12;
+
+/*
+ * Generics:
+ * ---------
+ * https://en.wikibooks.org/wiki/Java_Programming/Generics
+ */
+
+public class Launcher {
+
+	public static void main(String[] args) {
+
+	}
+
+}
